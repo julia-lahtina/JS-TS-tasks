8597228e4199b9edeb07460919ee94ac1880020b
julia-lahtina <j.lakhtin@gmail.com>
2024-02-03 14:00:22 +0100

    add functions

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {SplitIntoWords} from "./01-hello-tests/01";
+
+const sentence = "Hello my friends!";
+const result = SplitIntoWords(sentence)
+console.log(result[0] === "hello")
+console.log(result[1] === "my")
+console.log(result[2] === "friend")
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
