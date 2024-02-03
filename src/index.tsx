8ee8979192581e87d6382ca662335a71cb8ee2f4
julia-lahtina <j.lakhtin@gmail.com>
2024-02-03 19:00:47 +0100

    import sum

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {splitIntoWords} from "./01-hello-tests/01";
+import {sum} from "./03/03";
+
+let a = sum;
 
 const sentence = "Hello my friends!";
 const result = splitIntoWords(sentence)
