a7122c4295e7051dfeeaa7b8ce306a838baee09e
julia-lahtina <j.lakhtin@gmail.com>
2024-02-03 15:30:07 +0100

    add 02.test

---
+++ src/02/02.ts
@@ -0,0 +1,60 @@
+type StudentType = {
+    id: number
+    name: string
+    age: number
+    isActive: boolean
+    address: AdsressType
+    technologies: Array<TechType>
+}
+
+type AdsressType = {
+    streetTitle: string
+    city: CityType
+}
+
+type CityType = {
+    title: string
+    country: string
+}
+
+type TechType = {
+    id: number
+    title: string
+}
+
+
+const student: StudentType = {
+    id: 1,
+    name: "John",
+    age: 31,
+    isActive: false,
+    address: {
+        streetTitle: "Surganova 2",
+        city: {
+            title: "Minsk",
+            country: "Belarus"
+        }
+    },
+    technologies: [
+        {
+           id: 1,
+           title: "HTML"
+        },
+
+        {
+           id: 2,
+           title: "CSS"
+        },
+
+        {
+           id: 3,
+           title: "React"
+        },
+
+    ]
+}
+
+console.log(student.age)
+console.log(student.name)
+console.log(student.address.city.title)
+console.log(student.technologies[2].title)
