a009964e403e404f6f94b0ecbdf80a0aba60f46f
julia-lahtina <j.lakhtin@gmail.com>
2024-02-08 17:26:05 +0100

    add method map

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,23 @@ import './App.css';
 function App() {
 
     const names = ["Dima", "Sveta", "Katja", "Viktor"]
-    const liElements = names.map(n => <li>{n}</li>)
+    const users = [
+        {name: "Dima"},
+        {name: "Sveta"},
+        {name: "Katja"},
+        {name: "Viktor"}
+    ]
+    const liElements = names.map((n, index) => <li key={index}>{n}</li>)
+    const liUsers = users.map((u, index) => <li key={index}>{u.name}</li>)
 
     return (
         <div className="App">
             <ul>
                 {liElements}
             </ul>
+          <ul>
+            {liUsers}
+          </ul>
         </div>
     );
 }
