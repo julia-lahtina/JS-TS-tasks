75610476b90da442738fa25b9249485c50d8fd16
julia-lahtina <j.lakhtin@gmail.com>
2024-02-08 17:14:49 +0100

    add method map

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,17 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
 
-    </div>
-  );
+    const names = ["Dima", "Sveta", "Katja", "Viktor"]
+    const liElements = names.map(n => <li>{n}</li>)
+
+    return (
+        <div className="App">
+            <ul>
+                {liElements}
+            </ul>
+        </div>
+    );
 }
 
 export default App;
